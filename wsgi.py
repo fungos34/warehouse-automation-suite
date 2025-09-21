7d5e73e15966685023b0a7a8c14599b6095a8c66
fungos34 <114747128+fungos34@users.noreply.github.com>
2025-09-21 23:34:52 +0200

    refactor: Replace asgi2wsgi with ASGIMiddleware for improved ASGI to WSGI adaptation

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
-from asgi2wsgi import asgi2wsgi
+from a2wsgi import ASGIMiddleware
 from main import app  # FastAPI app
 
 # adapter wraps the ASGI app into a WSGI callable
-application = asgi2wsgi(app)
+application = ASGIMiddleware(app)
